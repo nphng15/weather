1da8846e18a8937a9fbc4bbb10eafe9a6fc30bef
Tran Nam Phong <phong98753@gmail.com>
2025-02-12 00:34:37 +0700

    update: code format

--- lib/widgets/mini_forecast_card.dart
+++ lib/widgets/mini_forecast_card.dart
@@ -24,7 +24,6 @@ class MiniForecastCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      // color: Colors.amber,
       child: Container(
         margin: EdgeInsets.only(top: 10),
         width: MediaQuery.of(context).size.width*0.9,
